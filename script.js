89aeeffa843636618f4ccba7b4d23a63b09846be
Mark Luo <mluo9987@gmail.com>
2024-12-29 13:24:23 -0500

    Added basic calculator operations

---
+++ script.js
@@ -0,0 +1,25 @@
+var x = 4;
+var y = 3;
+
+// Addition
+function add(x, y) {
+    return x + y;
+}
+console.log(add(x, y));
+
+// Subtraction
+function subtract(x, y) {
+    return x - y;
+}
+console.log(subtract(x, y));
+
+// Multiplication
+function multiply(x, y) {
+    return x * y;
+}
+console.log(multiply(x, y));
+
+function divide(x, y) {
+    return x / y;
+}
+console.log(divide(x, y));
